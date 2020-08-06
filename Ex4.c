7f9e8a7721d7c78e970292ba63f02ec46326b723
s1270224 <69284971+s1270224@users.noreply.github.com>
2020-08-06 12:48:16 +0900

    Final revision

--- Ex4.c
+++ Ex4.c
@@ -25,8 +25,8 @@ int main(){
   
   printf("Heads: %d, Tails: %d\n",heads,tails);
   
-  if(heads>tails) printf("You won\n");
-  else printf("You lost\n");
+  if(heads>tails) printf("%s won\n",str);
+  else printf("%s lost\n",str);
   
   return 0;
 }
